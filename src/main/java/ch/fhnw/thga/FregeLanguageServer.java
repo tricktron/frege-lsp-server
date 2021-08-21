1519b4d4b8954c447de46ffa8c1074ea376edb1c
tricktron <tgagnaux@gmail.com>
2021-08-21 19:29:13 +0200

    Adds Type Signature Hover and Compiler Error Diagnostic Capabilities (#2)
    * feat: WIP: trying to understand the `FregeInterpreter`
    
    * refactor: Simplifies test
    
    Starts with type infering simple expressions instead of complete functions.
    
    * refactor: uses the `frege-repl` to infer the type
    
    * refactor: uses junit5 `nested` and `parameterizedTest` capabilites
    
    * feat: publishes compiler errors on `didOpen` and `didSave` events
    
    * refactor: Removes proof of concept completion and renames files
    
    * refactor: removes completion and adds hover capability
    
    * fix: Adds utf-8 encoding java compile option and inits the `TextDocumentItem` before each test
    
    * fix: adds a timeout for async tests
    
    Co-authored-by: Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>

--- src/main/java/ch/fhnw/thga/FregeLanguageServer.java
+++ src/main/java/ch/fhnw/thga/FregeLanguageServer.java
@@ -2,34 +2,32 @@ package ch.fhnw.thga;
 
 import java.util.concurrent.CompletableFuture;
 
-import org.eclipse.lsp4j.CompletionOptions;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
 import org.eclipse.lsp4j.ServerCapabilities;
 import org.eclipse.lsp4j.TextDocumentSyncKind;
 import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
 import org.eclipse.lsp4j.services.LanguageServer;
 import org.eclipse.lsp4j.services.TextDocumentService;
 import org.eclipse.lsp4j.services.WorkspaceService;
 
-public class SimpleLanguageServer implements LanguageServer {
+public class FregeLanguageServer implements LanguageServer, LanguageClientAware {
 
 	private TextDocumentService textService;
 	private WorkspaceService workspaceService;
 	LanguageClient client;
 
-	public SimpleLanguageServer() {
-		textService = new SimpleTextDocumentService(this);
-		workspaceService = new SimpleWorkspaceService();
+	public FregeLanguageServer() {
+		textService = new FregeTextDocumentService(this);
+		workspaceService = new FregeWorkspaceService();
 	}
 
 	@Override
 	public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
 		final InitializeResult res = new InitializeResult(new ServerCapabilities());
-		CompletionOptions completionProvider = new CompletionOptions();
-		completionProvider.setResolveProvider(Boolean.TRUE);
-		res.getCapabilities().setCompletionProvider(completionProvider);
 		res.getCapabilities().setTextDocumentSync(TextDocumentSyncKind.Full);
+		res.getCapabilities().setHoverProvider(true);
 		return CompletableFuture.supplyAsync(() -> res);
 	}
 
@@ -53,7 +51,8 @@ public class SimpleLanguageServer implements LanguageServer {
 		return this.workspaceService;
 	}
 
-	public void connectClient(LanguageClient client) {
+	@Override
+	public void connect(LanguageClient client) {
 		this.client = client;
 	}
 }
